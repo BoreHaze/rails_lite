3054b85ec5c3a8c7d7eb98dbaad918ea66f5b123
Maxfield Lewin <mslewin@gmail.com>
2015-06-23 14:43:15 -0400

    Phase 4, persisting cookies.

--- lib/phase4/controller_base.rb
+++ lib/phase4/controller_base.rb
@@ -4,13 +4,18 @@ require_relative './session'
 module Phase4
   class ControllerBase < Phase3::ControllerBase
     def redirect_to(url)
+      @session.store_session(self.res)
+      super(url)
     end
 
     def render_content(content, content_type)
+      @session.store_session(self.res)
+      super(content, content_type)      
     end
 
     # method exposing a `Session` object
     def session
+      @session ||= Session.new(self.req)
     end
   end
 end
