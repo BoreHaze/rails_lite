806936141193b5147567bd0b9279a9f03b702086
Maxfield Lewin <mslewin@gmail.com>
2015-06-23 13:54:36 -0400

    Phase 2, basic request/response handling.

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,30 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      self.res.status = 302
+      self.res.header['location'] = url
+      self.render_content(nil, nil)
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      self.res.content_type = content_type
+      raise RuntimeError if already_built_response?
+      self.res.body = content
+      @already_built_response = true
     end
   end
 end
