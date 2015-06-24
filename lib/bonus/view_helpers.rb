0e0c50e30a2c8d517de18a7cefdfc27794bea562
Maxfield Lewin <mslewin@gmail.com>
2015-06-24 14:51:58 -0400

    ViewHelper#link_to and #button_to implemented, tests forthcoming.

--- lib/bonus/view_helpers.rb
+++ lib/bonus/view_helpers.rb
@@ -1,15 +1,30 @@
+require 'erb'
+
 module ViewHelper
+
   def link_to(name, url)
 
-    link_url = "localhost:3000/#{url}"
+    link_url = "#{url}"
     #Check that route is valid? Need access to router from controller?
 
-    <<-HTML.html_safe
-      <a href=#{link_url}>#{name}</a>
+    <<-HTML
+<a href=#{link_url}>#{name}</a>
     HTML
   end
 
-  def button_to(name, url)
+  def button_to(name, url, method = nil)
 
+    if %w(delete put patch).include?(method.downcase)
+      hidden_method = <<-HTML
+<input type='hidden' name='_method' value='#{method}'>
+      HTML
+    end
+
+    <<-HTML
+<form action='#{url}' method='POST'>
+  #{hidden_method unless hidden_method.nil?}
+  <input value='#{name}' type='submit'>
+</form>
+    HTML
   end
 end
---
+++ spec/bonus_view_helpers_spec.rb
@@ -0,0 +1,4 @@
+require 'webrick'
+require_relative '../lib/bonus/controller_base'
+require_relative '../lib/phase6/router'
+require 'byebug'
